1b158324565bf100e86eabf51d5005e7ae6d5ee6
Arthur <arthur.hamel1@orange.fr>
2021-06-22 09:36:11 +0200

    add return back btn on rando page

--- src/components/RandoPage/index.js
+++ src/components/RandoPage/index.js
@@ -1,12 +1,21 @@
 import FavorisIcon from "../../assets/icons/favoris.png";
 import RandoPodcast from "../RandoPodcast";
 import RandoReportage from "../RandoReportage";
+import ReturnIcon from "../../assets/icons/return.png";
 import "./styles.css";
+import { useHistory } from "react-router";
 
 export default function Rando({ data }) {
+  const history = useHistory();
+
+  const returnBack = () => {
+    history.goBack(-1);
+  };
+
   return (
     <div id="rando">
       <div className="header">
+        <img src={ReturnIcon} alt="return back" onClick={returnBack} />
         <img src={`/assets/images/${data.img}`} alt={data.title} />
       </div>
       <div className="content">
